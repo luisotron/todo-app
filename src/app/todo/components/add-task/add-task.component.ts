f72202a8a09ef94cae14d9ed4ed1e43e2f00c301
luisotron <luiso_tron@hotmail.com>
2024-04-26 19:45:32 +0200

    Aplicaicon mejorada

---
+++ src/app/todo/components/add-task/add-task.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Task } from '../../interfaces/task.interface';
+
+@Component({
+  selector: 'todo-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrl: './add-task.component.css'
+})
+export class AddTaskComponent {
+  @Output()
+  public onNewTask: EventEmitter<Task> =new EventEmitter();
+  
+public task: Task = {
+  description: '',
+  isCompleted: false
+};
+
+  public emitTask(): void {
+    this.onNewTask.emit(this.task);
+    this.task = {
+      description: '',
+      isCompleted: false
+    };
+  };
+
+}
--- src/app/todo/components/todo/todo.component.ts
+++ src/app/todo/components/todo/todo.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
 
+import { Task } from '../../interfaces/task.interface';
+
 @Component({
   selector: 'todo-root',
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
 export class TodoComponent {
+  public tasks: Array<Task>/*Task[]*/ =[
+    {
+      description: "Inicializar Aplicacion TODO",
+      isCompleted: true
+    },
+    {
+      description: "Crear Aplicacion TODO",
+      isCompleted: false
+    },
+    {
+      description: "Actualizar modulo TODO",
+      isCompleted: false
+    },
+    {
+      description: "Crear primer componente",
+      isCompleted: false
+    },
+    {
+      description: "Crear componente para lista",
+      isCompleted: false
+    },
+    {
+      description: "Finalizar aplicación",
+      isCompleted: false
+    }
+  ];
+  public onDeleteTask(index:number): void{
+    this.tasks.splice(index, 1);
+  }
+  public onCompleteTask(index:number): void{
+      this.tasks[index].isCompleted= !this.tasks[index].isCompleted;
+  }
+  public onNewTask(task :Task): void{
+    this.tasks.push(task);
+}
 
 }
